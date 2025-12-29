fd27523e3b89b39b2efb7ec535825adba4c54ec2
HARISH MAHENDRA PAWAR <pawarharish2801@gmail.com>
2025-12-29 13:32:20 +0530

    Add files via upload

---
+++ SequenceX.py
@@ -0,0 +1,16 @@
+#LIST   DUPLICATE
+Value1 = [10,20,30,40,10]  
+print(Value1[0])  
+Value1[2]=35    
+print(Value1[2])   
+
+#TUPLE  DUPLICATE
+Value2 = (10,20,30,40,10)  
+print(Value2[0]) 
+#Value2[2]=35               ERROR 
+print(Value2[2]) 
+
+#SET    NO DUPLICATE
+Value3 = {10,20,30,40,10}     
+#print(Value3[0])           #Error set doesnt have indexes    
+
